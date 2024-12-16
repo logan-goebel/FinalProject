b3bc303a2d689998a3ecc44071bf7b23090c5f42
GrantHinze <Granthinze@ndus.edu>
2024-12-15 20:38:43 -0600

    DogList

--- Test2/migration_script.sql
+++ Test2/migration_script.sql
@@ -13,194 +13,95 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
+    WHERE [MigrationId] = N'20241215204502_mssql.local_migration_850'
 )
 BEGIN
-    CREATE TABLE [AspNetRoles] (
-        [Id] nvarchar(450) NOT NULL,
-        [Name] nvarchar(256) NULL,
-        [NormalizedName] nvarchar(256) NULL,
-        [ConcurrencyStamp] nvarchar(max) NULL,
-        CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE TABLE [AspNetUsers] (
-        [Id] nvarchar(450) NOT NULL,
-        [UserName] nvarchar(256) NULL,
-        [NormalizedUserName] nvarchar(256) NULL,
-        [Email] nvarchar(256) NULL,
-        [NormalizedEmail] nvarchar(256) NULL,
-        [EmailConfirmed] bit NOT NULL,
-        [PasswordHash] nvarchar(max) NULL,
-        [SecurityStamp] nvarchar(max) NULL,
-        [ConcurrencyStamp] nvarchar(max) NULL,
-        [PhoneNumber] nvarchar(max) NULL,
-        [PhoneNumberConfirmed] bit NOT NULL,
-        [TwoFactorEnabled] bit NOT NULL,
-        [LockoutEnd] datetimeoffset NULL,
-        [LockoutEnabled] bit NOT NULL,
-        [AccessFailedCount] int NOT NULL,
-        CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE TABLE [AspNetRoleClaims] (
+    CREATE TABLE [ContactForm] (
         [Id] int NOT NULL IDENTITY,
-        [RoleId] nvarchar(450) NOT NULL,
-        [ClaimType] nvarchar(max) NULL,
-        [ClaimValue] nvarchar(max) NULL,
-        CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
-        CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+        [Email] nvarchar(max) NOT NULL,
+        [QuestionsAndConcerns] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_ContactForm] PRIMARY KEY ([Id])
     );
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE TABLE [AspNetUserClaims] (
-        [Id] int NOT NULL IDENTITY,
-        [UserId] nvarchar(450) NOT NULL,
-        [ClaimType] nvarchar(max) NULL,
-        [ClaimValue] nvarchar(max) NULL,
-        CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
-        CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE TABLE [AspNetUserLogins] (
-        [LoginProvider] nvarchar(450) NOT NULL,
-        [ProviderKey] nvarchar(450) NOT NULL,
-        [ProviderDisplayName] nvarchar(max) NULL,
-        [UserId] nvarchar(450) NOT NULL,
-        CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
-        CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE TABLE [AspNetUserRoles] (
-        [UserId] nvarchar(450) NOT NULL,
-        [RoleId] nvarchar(450) NOT NULL,
-        CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
-        CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
-        CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE TABLE [AspNetUserTokens] (
-        [UserId] nvarchar(450) NOT NULL,
-        [LoginProvider] nvarchar(450) NOT NULL,
-        [Name] nvarchar(450) NOT NULL,
-        [Value] nvarchar(max) NULL,
-        CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
-        CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
-    );
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    EXEC(N'CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL');
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
-)
-BEGIN
-    EXEC(N'CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL');
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema'
+    WHERE [MigrationId] = N'20241215204502_mssql.local_migration_850'
 )
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'00000000000000_CreateIdentitySchema', N'8.0.8');
+    VALUES (N'20241215204502_mssql.local_migration_850', N'8.0.11');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241215214605_mssql.local_migration_461'
+)
+BEGIN
+    CREATE TABLE [Form] (
+        [Id] int NOT NULL IDENTITY,
+        [FullName] nvarchar(max) NOT NULL,
+        [Email] nvarchar(max) NOT NULL,
+        [PhoneNumber] int NOT NULL,
+        [Address] nvarchar(max) NOT NULL,
+        [City] nvarchar(max) NOT NULL,
+        [State] nvarchar(max) NOT NULL,
+        [HouseSize] nvarchar(max) NOT NULL,
+        [NumberOfPets] int NOT NULL,
+        [PreviousDogOwner] bit NOT NULL,
+        [QuestionsConcerns] nvarchar(max) NULL,
+        CONSTRAINT [PK_Form] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241215214605_mssql.local_migration_461'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20241215214605_mssql.local_migration_461', N'8.0.11');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241216022049_mssql.local_migration_816'
+)
+BEGIN
+    CREATE TABLE [DogList] (
+        [Id] int NOT NULL IDENTITY,
+        [DogName] nvarchar(max) NULL,
+        [DogAge] int NOT NULL,
+        [DogDescription] nvarchar(max) NULL,
+        [DogBreed] nvarchar(max) NULL,
+        CONSTRAINT [PK_DogList] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241216022049_mssql.local_migration_816'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20241216022049_mssql.local_migration_816', N'8.0.11');
 END;
 GO
 
